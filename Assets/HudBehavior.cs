0de1aa54f355aec9757f67b6cfd1dc77173da081
Tianrui Zhang <tz85@duke.edu>
2020-05-15 14:42:51 -0400

    initial commit

---
+++ Assets/HudBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+using System.Net;
+using System;
+
+public class HudBehavior : MonoBehaviour
+{
+    public int Interval = 5;
+    public GameObject toolrep;
+    public GameObject mcamera;
+    public GameObject tool;
+    private float SecondsElapsed = 0.0f;
+    private string IPstr = "";
+    private bool DisplayTool = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        String strHostName = System.Net.Dns.GetHostName();
+        IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(strHostName);
+        var addr = ipEntry.AddressList;
+        IPstr = "IP: " + addr[addr.Length - 1].ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SecondsElapsed += Time.deltaTime;
+        if (SecondsElapsed <= Interval * 1)
+        {
+            GetComponent<TextMeshPro>().SetText(IPstr + "\nProceed to open space" + (Interval * 1 - SecondsElapsed).ToString());
+        }
+        else if (SecondsElapsed <= Interval * 2)
+        {
+            GetComponent<TextMeshPro>().SetText("Align tool with displayed object" + (Interval * 2 - SecondsElapsed).ToString());
+            if (!DisplayTool) {
+                toolrep.transform.position = mcamera.transform.position + mcamera.transform.forward * 0.4f + mcamera.transform.up * -0.2f;
+                toolrep.transform.rotation = toolrep.transform.rotation * mcamera.transform.rotation;
+                DisplayTool = true;
+            }
+        }
+        else 
+        {
+            toolrep.transform.SetParent(tool.transform);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/UDPReceiver.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceiver : MonoBehaviour
+{
+    Thread receiveThread;
+    UdpClient client;
+    public int port; // define > init
+    public GameObject brain;
+    public GameObject tool;
+    public GameObject hud;
+    private Dictionary<string, string> updates;
+    private Dictionary<string, GameObject> objects;
+    private string toolStr = "";
+    private string brainStr = "";
+
+    private void init()
+    {
+        updates = new Dictionary<string, string>();
+        objects = new Dictionary<string, GameObject>();
+        objects.Add("brain", brain);
+        objects.Add("tool", tool);
+        port = 8051;
+        print("Sending to 127.0.0.1 : " + port);
+        print("Test-Sending to this Port: nc -u 127.0.0.1  " + port + "");
+
+        receiveThread = new Thread(
+            new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+    }
+
+    private void ReceiveData()
+    {
+        client = new UdpClient(port);
+        while (true)
+        {
+            try
+            {
+                // Bytes empfangen.
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+                string text = Encoding.UTF8.GetString(data);
+                // print(">> " + text);
+                // decoding
+                if (text.Length == 0)
+                {
+                    continue;
+                }
+                text = text.Substring(0, text.Length - 1);
+                toolStr = text.Split('#')[0];
+                brainStr = text.Split('#')[1];
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    void Start()
+    {
+        init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toolStr != "")
+        {
+            var pos = toolStr.Split(':')[0].Split(',');
+            Vector3 toolPos = new Vector3(float.Parse(pos[0]), float.Parse(pos[1]), float.Parse(pos[2]));
+            var rot = toolStr.Split(':')[1].Split(',');
+            Quaternion toolRot = new Quaternion(float.Parse(rot[0]), float.Parse(rot[1]), float.Parse(rot[2]), float.Parse(rot[3]));
+            tool.transform.localPosition = toolPos;
+            tool.transform.localRotation = toolRot;
+
+            pos = brainStr.Split(':')[0].Split(',');
+            Vector3 brainPos = new Vector3(float.Parse(pos[0]), float.Parse(pos[1]), float.Parse(pos[2]));
+            rot = brainStr.Split(':')[1].Split(',');
+            Quaternion brainRot = new Quaternion(float.Parse(rot[0]), float.Parse(rot[1]), float.Parse(rot[2]), float.Parse(rot[3]));
+            brain.transform.localPosition = brainPos;
+            brain.transform.localRotation = brainRot;
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        receiveThread.Abort();
+    }
+}
